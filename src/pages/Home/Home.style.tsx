12b6d1de452a1202ee5a2937175a52fe082a46b0
Jusley Tavares <jusleyarley@hotmail.com>
2025-10-22 16:11:49 -0300

    refact: change style from home

--- src/pages/Home/Home.style.tsx
+++ src/pages/Home/Home.style.tsx
@@ -2,29 +2,6 @@ import styled, { css } from 'styled-components';
 
 import { ReactComponent as Gradient } from '../../assets/images/icons/gradient.svg';
 
-export const GradientImage = styled(Gradient)`
-  top: 900px;
-  width: auto;
-  height: 800px;
-  position: absolute;
-  right: calc(100vw - 330px);
-
-  @media (max-width: 920px) {
-    right: calc(100vw - 230px);
-  }
-`;
-
-export const HeaderBanner = styled.div`
-  display: flex;
-  height: 508px;
-  grid-area: banner;
-  flex-direction: column;
-
-  @media (max-width: 920px) {
-    height: 399px;
-  }
-`;
-
 export const Wrapper = styled.div<{ typeMode: string }>`
   gap: 124px;
   display: grid;
@@ -34,9 +11,31 @@ export const Wrapper = styled.div<{ typeMode: string }>`
     'language'
     'experience'
     'footer';
-
   ${({ theme: { colors }, typeMode }) => css`
     color: ${colors[`${typeMode}`].font};
     background-color: ${colors[`${typeMode}`].background};
   `}
+`;
+
+export const GradientImage = styled(Gradient)`
+  top: 600px;
+  position: absolute;
+  right: calc(100vw - 460px);
+
+  @media (min-width: 1900px) {
+    right: calc(100vw - 660px);
+  }
+  @media (max-width: 920px) {
+    right: calc(100vw - 350px);
+  }
+`;
+
+export const HeaderBanner = styled.div`
+  display: flex;
+  height: 508px;
+  grid-area: banner;
+  flex-direction: column;
+  @media (max-width: 920px) {
+    height: 399px;
+  }
 `;
